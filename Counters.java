5cdbdff88326edb5fa230d60b3efd19ad384aa62
kavinkumar06 <kavinm09@gmail.com>
2017-07-31 12:00:47 +0530

    Create Counters.java

---
+++ Counters.java
@@ -0,0 +1,21 @@
+# Counters
+import java.io.*;
+import java.util.*;
+public class Counters
+{
+  public static void main(String args[])
+  {
+   int a,b,c;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		System.out.println("The subtraction is");
+		c=a-b;
+		System.out.println(c);
+
+
+	}
+
+}
